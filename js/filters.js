c7097b287508542f3ac0c414479ca81dcccf9b0e
Daria Egarmina <d.egarmina@gmail.com>
2024-07-30 19:30:27 +0300

    Начинает делать маленькие функции для фильтрации

--- js/filters.js
+++ js/filters.js
@@ -1,13 +1,15 @@
 import { setDisabledState } from './util.js';
 import { renderSimilarMarkers } from './map.js';
 
+
 const SIMILAR_MARKERS_COUNT = 10;
 
 const mapFiltersForm = document.querySelector('.map__filters');
 const filterElements = mapFiltersForm.querySelectorAll('.map__filter');
 const featuresElement = mapFiltersForm.querySelector('.map__features');
 
-const housingTypeSelectElement = document.querySelector('#housing-type');
+const typeSelectElement = document.querySelector('#housing-type');
+//const priceSelectElement = document.querySelector('#housing-price');
 
 const disableFilters = () => {
   mapFiltersForm.classList.add('map__filters--disabled');
@@ -33,18 +35,46 @@ const resetFilters = () => mapFiltersForm.reset();
 
 //Фильтрация по типу жилья
 
+// const filterByType = (ad) => {
+//   const value = typeSelectElement.value;
+//   if (value === 'any') {
+//     return true;
+//   }
+//   return value === ad.offer.type;
+// };
+
 const setFilterByType = (ads) => {
-  housingTypeSelectElement.addEventListener('change', () => {
-    const currentSelectValue = housingTypeSelectElement.options[housingTypeSelectElement.selectedIndex].value;
+  typeSelectElement.addEventListener('change', () => {
     const filteredAds = ads.filter((ad) => {
-      if (currentSelectValue === 'any') {
+      if (typeSelectElement.value === 'any') {
         return ad;
       }
-      return ad.offer.type === currentSelectValue;
+      return ad.offer.type === typeSelectElement.value;
     });
     renderSimilarMarkers(filteredAds.slice(0, SIMILAR_MARKERS_COUNT));
   });
 };
+
+//Фильтрация по цене
+
+// const setFilterByPrice = (ads) => {
+//   priceSelectElement.addEventListener('change', () => {
+//     const currentSelectValue = priceSelectElement.options[priceSelectElement.selectedIndex].value;
+//     const filteredAds = ads.filter((ad) => {
+//       if (currentSelectValue === 'middle') {
+//         return ad.offer.price >= 10000 && ad.offer.price <= 50000;
+//       } else if (currentSelectValue === 'low') {
+//         return ad.offer.price < 10000;
+//       } else if (currentSelectValue === 'high') {
+//         return ad.offer.price > 50000;
+//       } else {
+//         return ad;
+//       }
+//     });
+//     renderSimilarMarkers(filteredAds.slice(0, SIMILAR_MARKERS_COUNT));
+//   });
+// };
+
 
 const initFilters = (ads) => {
   enableFilters();
